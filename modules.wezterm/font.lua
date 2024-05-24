b024f7f1dd937e5dc86a1de152396464a7e30138
wingedseraph <r89647@gmail.com>
2024-05-24 17:28:08 +0300

    add: new fonts for wezterm modules

--- modules.wezterm/font.lua
+++ modules.wezterm/font.lua
@@ -12,9 +12,23 @@ function M.set_font(config)
 	-- 		}),
 	-- 	},
 	-- }
+	-- config.font = wezterm.font({ family = "Brutalist Mono" })
+	-- config.font = wezterm.font({ family = "Anonymous Pro", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Dank Mono", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "VictorMono Nerd Font Mono", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "MonoLisa Nerd Font Mono", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Monaspace Krypton", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Monaspace Argon", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Monaspace Neon", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Monaspace Xenon" })
 	-- config.font = wezterm.font({ family = "IosevkaTerm Nerd Font" })
-	-- config.font = wezterm.font({ family = "JetBrainsMono Nerd Font" })
-	config.font = wezterm.font({ family = "BlexMono Nerd Font", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "IosevkaTerm Nerd Font" })
+	-- config.font = wezterm.font({ family = "CozetteVector", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "Operator Mono Light", weight = "Regular" })
+	config.font = wezterm.font({ family = "JetBrainsMono Nerd Font" })
+	-- config.font = wezterm.font({ family = "DotGothic16" })
+	-- config.font = wezterm.font({ family = "BlexMono Nerd Font", weight = "Regular" })
+	-- config.font = wezterm.font({ family = "CaskaydiaCove NF" })
 
 	config.cell_width = 0.9 -- like alacritty
 	-- config.line_height = 1.35
