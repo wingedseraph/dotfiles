26c1b23a9c009d4445e0a44b0d7fb0796daff0a7
wingedseraph <r89647@gmail.com>
2024-05-28 11:27:37 +0300

    feat: neovim better colorscheme file and new colorschemes :)

--- nvchad/lua/misc/default_colorschemes_fix.lua
+++ nvchad/lua/misc/default_colorschemes_fix.lua
@@ -15,10 +15,18 @@ highlight TabLine ctermfg=none ctermbg=none
 highlight TabLineFill ctermfg=DarkGrey ctermbg=none
 highlight TabLineSel ctermfg=White ctermbg=none
 highlight Visual ctermfg=none ctermbg=Black
+hi LineNr guifg=NONE guibg=NONE
+hi clear Float
+hi clear NonText
+hi clear NormalFloat
+hi link NonText Normal
 ]])
-vim.cmd("hi LineNr guifg=NONE guibg=NONE")
+-- vim.cmd("hi LineNr guifg=NONE guibg=NONE")
+
 -- fix for all vim colorschemes
-vim.cmd("highlight clear Float")
-vim.cmd("highlight clear NormalFloat")
--- vim.cmd("highlight link Float Folded")
--- vim.cmd("highlight link NormalFloat Folded")
+-- vim.cmd.hi("clear Float")
+-- vim.cmd.hi("clear NonText")
+-- vim.cmd.hi("clear NormalFloat")
+-- vim.cmd.hi("link NonText Normal")
+-- vim.cmd.hi("link Float Folded")
+-- vim.cmd.hi("link NormalFloat Folded")
